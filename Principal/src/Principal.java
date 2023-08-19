62c2bb284b7902e1bebd113773e46cadef07365a
Alumno <Alumno@UCSM000533>
2023-08-19 09:11:13 -0600

    //holamundo

---
+++ Principal/src/Principal.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int vida,rp;
+		boolean fl=false;
+		vida=0;
+		Scanner sc=new Scanner(System.in);
+		do
+	}
+
+}
---
+++ principall/src/principall/principall.java
@@ -0,0 +1,9 @@
+package principall;
+
+public class principall {
+
+	public static void main(String[] args) {
+		System.out.println("holamundo");
+	}
+
+}
